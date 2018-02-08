e0b406a1fada6ca550a4cb920459a84143b84a7c
Ollikehy <eragah@gmail.com>
2018-02-08 21:30:26 +0200

    teht 4.4

--- viikko4/4.1blog/utils/list_helper.js
+++ viikko4/4.1blog/utils/list_helper.js
@@ -2,6 +2,17 @@ const dummy = (blogs) => {
     return 1
 }
 
+const totalLikes = (blogs) => {
+    let sum = 0
+    const reducer = (sum, blog) => {
+        return sum + blog.likes
+    }
+
+    sum = blogs.reduce(reducer, 0)
+    return sum
+}
+
 module.exports = {
-    dummy
+    dummy,
+    totalLikes
 }
