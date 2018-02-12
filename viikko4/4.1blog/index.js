7975f064f6f3c4e74ea67cf673d86d3f48c4cde4
Ollikehy <eragah@gmail.com>
2018-02-12 20:14:41 +0200

    teht 4.18

--- viikko4/4.1blog/index.js
+++ viikko4/4.1blog/index.js
@@ -5,12 +5,11 @@ const cors = require('cors')
 const mongoose = require('mongoose')
 const blogsRouter = require('./controllers/blogs')
 const usersRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
 const Blog = require('./models/blog')
 const User = require('./models/user')
 const config = require('./utils/config')
 const bodyParser = require('body-parser')
-
-
 mongoose.connect(config.mongoUrl)
 mongoose.Promise = global.Promise
 
@@ -19,6 +18,7 @@ app.use(bodyParser.json())
 
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
+app.use('/api/login', loginRouter)
 const server = http.createServer(app)
 
 server.listen(config.port, () => {
