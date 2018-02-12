4c03321d2c6d96101676cc471c20054ca8c4d81c
Ollikehy <eragah@gmail.com>
2018-02-12 20:39:09 +0200

    teht 4.20

--- viikko4/4.1blog/controllers/blogs.js
+++ viikko4/4.1blog/controllers/blogs.js
@@ -10,21 +10,12 @@ blogsRouter.get('/', async (request, response) => {
       
     response.json(blogs.map(Blog.formatBlog))
 })
-  
-const getTokenFrom = (request) => {
-  const authorization = request.get('authorization')
-  if (authorization && authorization.toLowerCase().startsWith('bearer')) {
-    return authorization.substring(7)
-  }
-  return null
-}
 
 blogsRouter.post('/', async (request, response) => {
   try {
     const body = request.body 
     
-    const token = getTokenFrom(request)
-    const decodedToken = jwt.verify(token, process.env.SECRET)
+    const decodedToken = jwt.verify(request.token, process.env.SECRET)
 
     if (!token || !decodedToken.id) {
       return response.status(401).json({error: 'token missing or invalid'})
--- viikko4/4.1blog/index.js
+++ viikko4/4.1blog/index.js
@@ -13,9 +13,19 @@ const bodyParser = require('body-parser')
 mongoose.connect(config.mongoUrl)
 mongoose.Promise = global.Promise
 
+const tokenExtractor = (request, response, next) => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.toLowerCase().startsWith('bearer')) {
+    request.token = authorization.substring(7)
+    next()
+  }
+  request.token = null
+  next()
+}
+
 app.use(cors())
 app.use(bodyParser.json())
-
+app.use(tokenExtractor)
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
 app.use('/api/login', loginRouter)
